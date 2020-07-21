5751898bc83ebc0287d83464f5bba9a635a8789f
joshuaparker <joshuanparker@gmail.com>
2020-07-20 22:11:15 -0400

    ptr to a template is a container is to which all the templates get parsed and are held.

--- version2/main.go
+++ version2/main.go
@@ -6,6 +6,7 @@ import (
 	"text/template"
 )
 
+//ptr to a template is a container is to which all the templates get parsed and are held.
 func main() {
 	tpl, err := template.ParseFiles("index.html")
 	if err != nil {
