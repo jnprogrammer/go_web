a6c7c3d966ee2f7d0508d686d65db407e9e6212a
joshuaparker <joshuanparker@gmail.com>
2020-07-20 21:20:48 -0400

    I've added required go branding as told by the gopher I dream of. . . .. I've created a simple template for making html files.

--- version1/main.go
+++ version1/main.go
@@ -1,5 +1,45 @@
-package version1
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
+	name := "Deamon"
+	tpl := `
+		<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Auto Gen HTML FTW let GO!</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <h1>` + name + `</h1>
+        <script src="" async defer></script>
+    </body>
+</html>
+		`
+	fmt.Sprint(tpl)
 
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal("Error creating file", err)
+	}
+	defer nf.Close()
+	io.Copy(nf, strings.NewReader(tpl))
+	fmt.Println(tpl)
 }
