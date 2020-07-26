caba666e0c56f77e495d0f505b61fd155da024a3
jnprogrammer <joshuanparker@gmail.com>
2020-07-25 23:58:12 -0400

    learning about compositit literals in templates

---
+++ version5/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html"))
+}
+
+func main() {
+
+}
